fbe422bc61b25ef0285b18ee79e29676e66dd186
DarkSky3309 <lust3309@gmail.com>
2023-05-16 16:02:33 +0300

    make custom hooks

--- src/hooks/useActions.ts
+++ src/hooks/useActions.ts
@@ -1,9 +1,10 @@
 import { useDispatch } from 'react-redux';
 import { useMemo } from 'react';
 import { bindActionCreators } from 'redux';
+import { rootActions } from '@/store/root-actions';
 
 export const useActions = () => {
   const dispatch = useDispatch();
 
-  return useMemo(() => bindActionCreators(allActions, dispatch), [dispatch]);
+  return useMemo(() => bindActionCreators(rootActions, dispatch), [dispatch]);
 }
