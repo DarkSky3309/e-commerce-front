19f6492028d49f5468a0b766a3b24795df22e6d5
DarkSky3309 <lust3309@gmail.com>
2023-06-01 14:19:32 +0300

    add total

--- src/components/header/components/headerCart/headerCart.tsx
+++ src/components/header/components/headerCart/headerCart.tsx
@@ -14,19 +14,25 @@ const HeaderCart = () => {
       <SquareButton Icon={RiShoppingCartLine}
                     number={items.length}
                     onClick={() => setIsShowing(!isShowing)} />
-      {isShowing && <div className={'absolute min-h-100 z-10 top-16 right-0 bg-black text-white rounded-md shadow-lg p-4'}>
-        <div className={'flex flex-col min-w-140 w-80 justify-between items-center  mt-6'}>
-          <h3 className={'text-lg font-bold absolute top-2 left-2'}>Cart</h3>
-          <div>{items.length ? (items.map(item => <CartItem item={item} key={item.id}/>)): (<span className={'font-light'}>Cart is empty</span>)}</div>
-          <button onClick={() => setIsShowing(false)} className={'absolute top-2 right-2'}>
-            <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6 text-gray-500 hover:text-gray-700'
-                 fill='none' viewBox='0 0 24 24' stroke='currentColor'>
-              <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2}
-                    d='M6 18L18 6M6 6l12 12' />
-            </svg>
-          </button>
-        </div>
-      </div>}
+      {isShowing &&
+        <div className={'absolute min-h-100 z-10 top-16 right-0 bg-black text-white rounded-md shadow-lg p-4'}>
+          <div className={'flex flex-col min-w-140 w-80 justify-between items-center  mt-6'}>
+            <h3 className={'text-lg font-bold absolute top-2 left-2'}>Cart</h3>
+            <div>{items.length ? (items.map(item => <CartItem item={item} key={item.id} />)) : (
+              <span className={'font-light'}>Cart is empty</span>)}</div>
+            <button onClick={() => setIsShowing(false)} className={'absolute top-2 right-2'}>
+              <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6 text-gray-500 hover:text-gray-700'
+                   fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+                <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2}
+                      d='M6 18L18 6M6 6l12 12' />
+              </svg>
+            </button>
+            <div className={'flex justify-center items-center w-full mt-4'}>
+              <span className={'text-lg font-bold'}>Total: </span>
+              <span className={'text-lg font-bold'}> {total}$</span>
+            </div>
+          </div>
+        </div>}
 
 
     </div>
