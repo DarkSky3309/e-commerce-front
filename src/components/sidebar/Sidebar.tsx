416934fc655adce4e11c064be1503383e1255edf
DarkSky3309 <lust3309@gmail.com>
2023-06-01 14:50:55 +0300

    create sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,10 +1,58 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { CategoryService } from '@/services/category.service';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import { useAuth } from '@/hooks/useAuth';
+import { logout } from '@/store/user/user.actions';
+import { FiLogOut } from 'react-icons/fi';
 
 const Sidebar = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const { data, isLoading } = useQuery(
+    ['get categories'],
+    () => CategoryService.getCategories(),
+    {
+      select: ({ data }) => data,
+    },
+  );
+  const { user } = useAuth();
+  const pathname = usePathname();
+  useEffect(() => {
+    setIsLoaded(true);
+  }, [data]);
+
   return (
-    <div className={'h-full bg-secondaryColor'}>
-      SIDEBAR
-    </div>
+    <aside className={'h-full bg-secondaryColor'}>
+      {(isLoaded) &&
+      <>
+        <div>
+          <div className={'text-xl text-white mt-4 mb-6 ml-6'}>
+            Categories:
+          </div>
+          <ul className={'text-white'}>
+            {data?.map((category) => (
+              <li key={category.id}>
+                <Link href={`/category/${category.slug}`}
+                      className={`block text-lg my-3 px-10 hover:text-themeColor
+                     transition-colors duration-200 
+                     ${pathname === `/category/${category.slug}` ? 'text-themeColor' : 'text-white'}`}>
+                  {category.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+
+        </div>
+        {!!user && (
+          <button onClick={logout} className={'text-white flex items-center'}>
+            <FiLogOut size={20} className={'mr-2'} />
+            <span>Logout</span>
+          </button>
+        )}
+      </>}
+    </aside>
   );
 };
 
