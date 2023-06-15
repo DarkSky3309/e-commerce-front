caf315d7c4fbe8e4993eeefe5d3eef23ac0b8c82
DarkSky3309 <lust3309@gmail.com>
2023-06-15 16:51:37 +0300

    make rating review

--- src/components/product/components/product-info/components/productInfoReview.tsx
+++ src/components/product/components/product-info/components/productInfoReview.tsx
@@ -3,52 +3,54 @@ import { ReviewService } from '@/services/review.service';
 import { useQuery } from '@tanstack/react-query';
 import { Rating } from 'react-simple-star-rating';
 
-const RatingBar: FC<{ percent: number }> = ({ percent }) => {
+const RatingBar: FC<{ percent: number, text: string }> = ({ percent, text }) => {
   return (
-    <div className={'relative w-full h-2 bg-gray-200 rounded'}>
-      <div className={'absolute h-full bg-theme rounded'} style={{ width: `${percent}%` }} />
+    <div className={'flex items-center gap-2'}><span>{text}</span>
+      <div className={'relative w-full h-2 bg-gray rounded'}>
+        <div className={'absolute h-full bg-themeColor transition rounded'} style={{ width: `${percent}%` }} />
+      </div>
+      <span>{percent}%</span>
     </div>
+
   );
 };
-const ProductInfoReview:FC<{id: string | number}>   = ({id}) => {
+const ProductInfoReview: FC<{ id: string | number }> = ({ id }) => {
   const { data } = useQuery(['review', id], () => ReviewService.getReviews(id as number),
     { select: ({ data }) => data });
   const [rating, setRating] = useState(Math.round(data ?
-    (data?.reduce((acc, reviews) => acc + reviews.rating, 0) / data.length) : 0
+    (data?.reduce((acc, reviews) => acc + reviews.rating, 0) / data.length) : 0,
   ));
-  return (
-    <div>
-      <h4 className={'font-bold text-xl mt-4'}>Reviews</h4>
-      <div>
-        <div>
-          <div className={'flex gap-2 items-center'}>
-            <span className={'text-8xl font-bold'}>{rating}</span>
-            <div className={'flex flex-col'}>
+  return (data ? (<div className={'item-review'}>
+    <h4 className={'font-bold text-xl mt-4'}>Reviews</h4>
+    <div className={'flex justify-between'}>
+      <div className={'flex gap-2 items-center'}>
+        <span className={'text-8xl font-bold'}>{rating}</span>
+        <div className={'flex flex-col'}>
             <span className={'font-bold'}>
               {rating} out of 5 stars
             </span>
-              <Rating className={'pb-1'}
-                      readonly
-                      initialValue={rating}
-                      SVGstyle={{ display: 'inline-block' }}
-                      size={18}
-                      allowFraction
-                      transition
-              />
-              <span>
+          <Rating className={'pb-1'}
+                  readonly
+                  initialValue={rating}
+                  SVGstyle={{ display: 'inline-block' }}
+                  size={18}
+                  allowFraction
+                  transition
+          />
+          <span>
               Based on <b>{data ? data.length : 0}</b> reviews
             </span>
-            </div>
-          </div>
-          <div>
-            <RatingBar percent={20} />
-          </div>
         </div>
-
       </div>
-
+      <div className={'w-1/3'}>
+        <RatingBar percent={data.filter((review) => review.rating === 5).length / data.length * 100 | 0} text={'5'} />
+        <RatingBar percent={data.filter((review) => review.rating === 4).length / data.length * 100 | 0} text={'4'} />
+        <RatingBar percent={data.filter((review) => review.rating === 3).length / data.length * 100 | 0} text={'3'} />
+        <RatingBar percent={data.filter((review) => review.rating === 2).length / data.length * 100 | 0} text={'2'} />
+        <RatingBar percent={data.filter((review) => review.rating === 1).length / data.length * 100 | 0} text={'1'} />
+      </div>
     </div>
-  );
+  </div>) : <></>);
 };
 
 export default ProductInfoReview;
